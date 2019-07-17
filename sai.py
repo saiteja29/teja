eb767fd67cd4215d02c53de8354145d85db33946
saiteja29 <44159659+saiteja29@users.noreply.github.com>
2019-07-17 09:38:22 +0530

    Update sai.py

--- sai.py
+++ sai.py
@@ -1,7 +1,7 @@
 x,y=input().split()
 z=abs(len(y)-len(x))
 for i in range(len(x)):
-    if(len(y)==1 and y[i] in u):
+    if(len(y)==1 and y[i] in x):
         break
     if(x[i]!=y[i]):
         z=z+1
